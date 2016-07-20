26a569542dabf834d980f5037c7b5a5b4e643405
Ted Tzuridis <tzuridis@gmail.com>
2016-07-20 16:12:08 -0400

    Almost done just need to add multiple addresses and phone numbers

--- js/app.js
+++ js/app.js
@@ -1,36 +1,40 @@
 $(document).ready(function() {
-	$('#NewPhone').click(function(event){
-		event.preventDefault();
-		alert("")
-	})
-	$('#NewAddress').click(function(event){
-		event.preventDefault();
-		alert("")
-	})
+    $('.PhoneNumberPlus').click(function(event) {
+        event.preventDefault();
+        if ($('#ShowContact ul').hide()) {
+            prompt("Insert another Phone Number:")
+            var NewPhoneSave = $('#PhoneinputBox').val()
+            contact.PhoneNumber.push(NewPhoneSave)
+        } else {
+            null
+        }
+    })
+    $('.AddressPlus').click(function(event) {
+        event.preventDefault();
+        if ($('#ShowContact ul').hide()) {
+            prompt("Insert another Address:")
+            var NewAddressSave = $('#PhoneinputBox').val()
+            contact.Address.push(NewAddressSave)
+        } else {
+            null
+        }
+
+    })
     $('#AddButton').click(function(event) {
         event.preventDefault();
         var contact = {
             FirstName: $('#FirstinputBox').val(),
             LastName: $('#LastinputBox').val(),
-            PhoneNumber: [], 
+            PhoneNumber: [],
             Address: [],
             City: $('#CityinputBox').val(),
             State: $('#StateinputBox').val(),
         };
 
-        contact.PhoneNumber.push($('#PhoneinputBox').val())
-        contact.Address.push($('#AddressinputBox').val())
-
 
         if (!$("#FirstinputBox:input").val() || !$("#PhoneinputBox:input").val()) { //Alert if first name or phone number isnt included
             alert("Please insert a First Name & Phone Number")
         } else {
-
-
-            //Create a link to the contacts at the bottom which is saved in #ShowContact ul
-
-            //+++-----Need to add link to saved object {save}----+++
-
 
             var save = {
                 FirstName: contact.FirstName,
@@ -40,20 +44,13 @@ $(document).ready(function() {
                 City: contact.City,
                 State: contact.State
             };
-            //save the entire new contact
 
-            entireList.push(save);
 
-             $('#ShowContact ul').append("<li id='" + (entireList.length - 1) + "'>" + "<h1>" +  save.FirstName + " " + save.LastName + "</h1>" + 'First Name:' + " " + save.FirstName + "</br>" + 'Last Name:' + " " + save.LastName + "</br>" + 'Phone Number:' + " " + " " + save.PhoneNumber + "</br>" + 'Address:' + " " + save.Address + "</br>" + 'City:' + " " + save.City + "</br>" + 'State:' + " " + save.State + "</li>");
+            entireList.push(save); //save the entire new contact
+
+            $('#ShowContact ul').append("<li id='" + (entireList.length - 1) + "'>" + "<h1>" + save.FirstName + " " + save.LastName + "</h1>" + 'First Name:' + " " + save.FirstName + "</br>" + 'Last Name:' + " " + save.LastName + "</br>" + 'Phone Number:' + " " + " " + save.PhoneNumber + "</br>" + 'Address:' + " " + save.Address + "</br>" + 'City:' + " " + save.City + "</br>" + 'State:' + " " + save.State + "</li>");
 
             $('#ContactsList ul').append("<li><a href='#' data-index='" + (entireList.length - 1) + "'>" + contact.FirstName + " " + contact.LastName + "</a></li>");
-
-
-
-            // push the saved object into the contact array of objects
-
-
-            //save the new contact in #ShowContact ul
 
             $('#FirstinputBox').val("");
             $('#LastinputBox').val("");
@@ -69,17 +66,30 @@ $(document).ready(function() {
     })
 
     $('body').on('click', 'div#ContactsList ul li a', function() {
-    	var index = $(this).data('index');
-    	$('li#' + index).show();
-    	$('li#' + index).siblings().hide();
-    	console.log(index)
+        var index = $(this).data('index');
+        $('li#' + index).show();
+        $('li#' + index).siblings().hide();
+        document.getElementById('Edit').style.display = 'inline-block';
+        document.getElementById('Delete').style.display = 'inline-block';
+        console.log(index)
     })
 
 
 });
 
+var entireList = []; //this saves the input from var contact
 
+$('#Edit').on('click', 'div#ContactsList ul li a', function() {
+    event.preventDefault();
+    $('#FirstinputBox').val();
+    $('#LastinputBox').val();
+    $('#PhoneinputBox').val();
+    $('#AddressinputBox').val();
+    $('#CityinputBox').val();
+    $('#StateinputBox').val();
+});
 
-var entireList = [];
+$('#Delete').on('click', 'button i.fa-fa-trash-o', function() {
+    $('div#ContactsList ul li a').remove();
 
-//this saves the input from var contact
+})
